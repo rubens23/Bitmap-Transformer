51f6c7dda1e56e256c353bff3e845bfde02f066b
Ruben Sarkisyan <rubens24@uw.edu>
2016-05-16 12:00:35 -0700

    working on assignment

--- bitmap/bitmap.js
+++ bitmap/bitmap.js
@@ -3,14 +3,16 @@ const fs = require('fs');
 const file = fs.readFileSync(__dirname + '/non-palette-bitmap.bmp');
 
 const headers = {};
+
 console.log(file.slice());
 
 console.log(file[0], file[1]);
 
-headers.type = file.toString('ascii', );
-headers.size = file.readUint32LE();
-headers.pixelStart = fileReadInt32LE();
+headers.type = file.toString('ascii', 0, 2);
+headers.size = file.readUInt32LE(2);
+headers.pixelStart = file.readUInt32LE(10);
+headers.colors = file.readUInt32LE(46);
 
-console.log(file.readUInt8());
+console.log('first color: ' + file[54]);
 
 console.log(headers);
--- bitmap/fs/read-bitmap-data.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-
-let bitmap = fs.readFileSync(__dirname + '/' + process.argv[2]);
-let bitmapData = {};
